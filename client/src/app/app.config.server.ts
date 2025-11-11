5bebc5cbe99c934a23edaaec7db19953765dc8a5
lina <ylinorueda@gmail.com>
2025-11-11 02:00:41 -0400

    instalacion-arreglo

--- client/src/app/app.config.server.ts
+++ client/src/app/app.config.server.ts
@@ -3,10 +3,12 @@ import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { provideZoneChangeDetection } from '@angular/core';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(withRoutes(serverRoutes)),
+    provideNoopAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true })
   ]
 };
