c48ab9f237ee3d15eab532b53d2e6e1b66ea9bc5
poli2 <frikositte@gmail.com>
2025-12-18 23:13:27 -0400

    btn personal añadido

--- client/src/app/components/personal/nuevo-personal-dialog/nuevo-personal-dialog.ts
+++ client/src/app/components/personal/nuevo-personal-dialog/nuevo-personal-dialog.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PersonalService } from '../../../services/personal';
+import { Personal } from '../../../models/personal.interface';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nuevo-personal-dialog',
   standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule,
     MatDialogModule,
     MatFormFieldModule,
@@ -27,12 +30,16 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class NuevoPersonalDialogComponent implements OnInit {
   form!: FormGroup;
+  esEdicion: boolean;
 
   constructor(
     private fb: FormBuilder,
     private personalService: PersonalService,
-    public dialogRef: MatDialogRef<NuevoPersonalDialogComponent>
-  ) {}
+    public dialogRef: MatDialogRef<NuevoPersonalDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Personal | null
+  ) {
+    this.esEdicion = !!this.data;
+  }
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -45,13 +52,22 @@ export class NuevoPersonalDialogComponent implements OnInit {
       email: [''],
       telefono: ['']
     });
+
+    if (this.esEdicion) this.form.patchValue(this.data!);
   }
 
   guardar(): void {
     if (this.form.invalid) return;
-    this.personalService.create(this.form.value).subscribe({
-      next: (res) => this.dialogRef.close(res),
-      error: (err) => alert('Error al crear: ' + JSON.stringify(err.error))
+
+    const payload: Personal = { ...this.form.value, id: this.data?.id };
+
+    const req$ = this.esEdicion
+      ? this.personalService.update(this.data!.id, payload)
+      : this.personalService.create(payload);
+
+    req$.subscribe({
+      next: res => this.dialogRef.close(res),
+      error: err => alert('Error: ' + JSON.stringify(err.error))
     });
   }
 
